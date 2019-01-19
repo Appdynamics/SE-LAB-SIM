89eb9715d77eba838b59fe299117349d59351967
Michi <michi.eng@gmail.com>
2019-01-19 16:10:25 +0100

    updates

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 docker exec -d appd-platform /bin/sh -c "/opt/appdynamics/platform/platform-admin/bin/platform-admin.sh start-platform-admin"
 sleep 30
-docker exec appd-platform /bin/sh -c "/opt/appdynamics/platform/platform-admin/bin/platform-admin.sh login -u admin -p Appdynamics123"
+docker exec appd-platform /bin/sh -c "/opt/appdynamics/platform/platform-admin/bin/platform-admin.sh login -u admin -p appdynamics"
 docker exec appd-platform /bin/sh -c "/opt/appdynamics/platform/platform-admin/bin/platform-admin.sh submit-job --service Controller --job mysql_start"
 docker exec appd-platform /bin/sh -c "/opt/appdynamics/platform/platform-admin/bin/platform-admin.sh submit-job --service Controller --job start"
 
